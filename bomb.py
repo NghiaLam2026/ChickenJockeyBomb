6f5de6d7761cfeb196981b76ddc7fdb9a171f8c1
Nghia Lam <nghiatlam03@gmail.com>
2025-04-17 18:54:01 -0400

    Cleaning up functionalities

--- bomb.py
+++ bomb.py
@@ -3,37 +3,47 @@ import threading
 import pygame
 import time
 import random
+import os, sys
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
+    return os.path.join(base_path, relative_path)
 
 pygame.init()
 pygame.mixer.init()
 root = tk.Tk()
 
-lava_chicken = pygame.mixer.Sound("assets\lavaChicken.wav")
-chicken_jocky = pygame.mixer.Sound("assets\chicken_jockey.wav")
+lava_chicken = pygame.mixer.Sound(resource_path("assets/lava_chicken.wav"))
+chicken_jocky = pygame.mixer.Sound(resource_path("assets/chicken_jockey.wav"))
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-def sound_spam():
+def lava_chicken_spam():
+    lava_Chicken_duration = lava_chicken.get_length()
     lava_chicken.play()
-    for i in range(40):
+    time.sleep(lava_Chicken_duration - 1)
+    root.quit()
+
+def chicken_jockey_spam():
+    for i in range(50):
         chicken_jocky.play()
         time.sleep(0.8)
 
 def label_spam():
-    for i in range(40):
+    for i in range(50):
         top = tk.Toplevel()
         top.geometry(f"300x200+{random.randint(0, screen_width - 300)}+{random.randint(0, screen_height - 200)}")
         top.title("I AM STEVE!!!")
         myMessage = tk.Label(top, text="CHICKEN JOCKEY!!!", font=("Fixedsys", 20))
         myMessage.pack(pady=70)
-        time.sleep(0.6)
+        time.sleep(0.8)
 
-def runBoth():
-    threading.Thread(target=sound_spam, daemon=True).start()
-    threading.Thread(target=label_spam, daemon=True).start()
+def run_all():
+    threading.Thread(target = lava_chicken_spam, daemon = True).start()
+    threading.Thread(target = chicken_jockey_spam, daemon = True).start()
+    threading.Thread(target = label_spam, daemon = True).start()
 
-# Run both when GUI starts
-runBoth()
+run_all()
 
-# Start the Tkinter loop
 root.mainloop()
